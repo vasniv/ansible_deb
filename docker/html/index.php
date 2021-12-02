b816a3d773d1062ca5c8f9d6e597cbc569f609ba
Vasily Bahteev <VasBah@mail.ru>
2021-12-02 17:23:36 +0500

    Script_01

---
+++ docker/html/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<a href='/phpmyadmin/index.php'>PhpMyAdmin_Service</a>";
+
+?>
---
+++ roles/db_setup/files/data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (           
+  message varchar(255) NOT NULL         
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
+  INSERT INTO test(message) VALUES('This Message Added From Ansible');
+  INSERT INTO test(message) VALUES('ReadMe');         
+  INSERT INTO test(message) VALUES('Script Work')
